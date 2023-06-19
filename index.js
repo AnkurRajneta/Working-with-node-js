8dd85a05da33f9cd9c1a8e501db15494eb25d58f
AnkurRajneta <ankur_207003@saitm.org>
2023-06-19 16:35:39 +0530

    updated file

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
+function calculateSum(counter){
+     var sum = 0;
+     for(var i = 0; i < counter; i++){
+         sum = sum + i;
+
+     }
+     return sum;
+    }
 function handleFirstRequest(req,res){
-     res.send('Working with node')
+    var calculatedSum = calculateSum(100);
+    console.log(calculatedSum);
+     var answer = "the sum is" + calculatedSum;
+     res.send(answer);
 }
 app.get('/', handleFirstRequest) 
 
