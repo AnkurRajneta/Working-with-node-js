f2b9a61ac33791257da2933cb5018d446a7de1ee
AnkurRajneta <ankur_207003@saitm.org>
2023-06-22 09:07:41 +0530

    updated file

--- index.js
+++ index.js
@@ -20,20 +20,32 @@ function calculateSum(counter){
      return sum;
     }
 function handleFirstRequest(req,res){
-    var calculatedSum = calculateSum(100);
-    console.log(calculatedSum);
+    var counter = req.query.counter; 
+    console.log(req.query.counter2);
+    console.log(req.query.counter3);
+    var calculatedSum = calculateSum(counter);
+    // console.log(calculatedSum);  
      var answer = "the sum is" + calculatedSum;
      res.send(answer);
 }
-app.get('/', handleFirstRequest) 
 
 
+function handleSecondRequest(req,res){
+    res.send('hello world');
+}
+app.get('/handleSum', handleFirstRequest)//app.get() method specifies a callback function that will be invoked whenever there is an HTTP GET request with a path ('/') relative to the site root. The callback function takes a request and a response object as arguments, and simply calls the send() method on the response to return the string "Hello World!".
+
+
+// app.get('/handleSum2',handleFirstRequest)
+// app.get('/handleSum3',handleFirstRequest)
+
+app.get('/handleSum2',handleSecondRequest);
 function started(){
     
         console.log(`Example app listening on port ${port}`)    
     }
 
-app.listen(port,started)
+app.listen(port,started)//app.listen() method to start a server on a port
 
 // function calculateSum(counter){
 //     var sum = 0;
