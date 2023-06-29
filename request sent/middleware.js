38e64f4b94c31726c606a63998d7f625fdeb05f2
AnkurRajneta <ankur_207003@saitm.org>
2023-06-29 21:06:13 +0530

    updated file

--- request sent/middleware.js
+++ request sent/middleware.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+let numberOfRequests = 0;
 
 //everytime firstly the control will reach to the middleware
 
 function middleware1(req,res,next){ 
-  console.log("from inside middleware" + req.headers.counter);
-  res.send("Error from inside middleware");//if next is not there then we can show an error
+  numberOfRequests = numberOfRequests + 1;  
+  console.log(numberOfRequests);
+//   res.send("Error from inside middleware");//if next is not there then we can show an error
 
-   //next();//it indicates that now the control can go to another functions also 
+   next();//it indicates that now the control can go to another functions also 
 }
 
 app.use(middleware1)//It is the way to register middleware so that we can use it
